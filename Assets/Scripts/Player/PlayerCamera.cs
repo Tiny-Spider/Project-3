28853bbd56228b48ca19a579de80bb133353649c
Jaron-LAPTOP <jaron.95@hotmail.com>
2015-03-19 09:14:43 +0100

    Player controller

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[ExecuteInEditMode]
+public class PlayerCamera : MonoBehaviour {
+
+    public Transform playerPosition;
+    public float cameraDistance = -10;
+    public float lerpSpeed;
+    public float yOffset;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPosition = playerPosition.position;
+        targetPosition.z += + cameraDistance;
+        targetPosition.y += yOffset;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed * Time.deltaTime);
+	}
+}
