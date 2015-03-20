c16df4dbf7bc4cd2b9e17cbd281d5e31031b8af8
Jaron-LAPTOP <jaron.95@hotmail.com>
2015-03-20 11:52:32 +0100

    Player jitter fixed

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -9,13 +9,17 @@ public class PlayerCamera : MonoBehaviour {
     public float cameraDistance = -10;
     public float lerpSpeed;
     public float yOffset;
+
+    private Vector3 velocity = Vector3.zero;
 	
 	// Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         Vector3 targetPosition = playerPosition.position;
         targetPosition.z += +cameraDistance;
         targetPosition.y += yOffset;
-        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, lerpSpeed * Time.fixedDeltaTime);
+
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, lerpSpeed);
 
 
     }
